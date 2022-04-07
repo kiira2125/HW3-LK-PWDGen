4e48e13e2fffd19ab86cac3c009b3bf9e7a69633
LK <lunakiira0911@gmail.com>
2022-04-06 19:46:42 -0700

    first changes to my HW3 O_o

--- script.js
+++ script.js
@@ -35,7 +35,33 @@ const lowerCharacters = [
 
 ];
 
-const upperCharacters = [A,B,C,,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,,X,Y,Z
+const upperCharacters = [
+  A,
+  B,
+  C,
+  D,
+  E,
+  F,
+  G,
+  H,
+  I,
+  J,
+  K,
+  L,
+  M,
+  N,
+  O,
+  P,
+  Q,
+  R,
+  S,
+  T,
+  U,
+  V,
+  W,
+  X,
+  Y,
+  Z
 
 ];
 
